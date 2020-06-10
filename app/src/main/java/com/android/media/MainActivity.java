6bc8c3763534d75f929beaa5f7e4a5a31c70e95d
reuicheng <ruichengliberty>
2020-06-10 15:05:08 +0800

    部分修改

--- app/src/main/java/com/android/media/MainActivity.java
+++ app/src/main/java/com/android/media/MainActivity.java
@@ -1,87 +1,110 @@
 package com.android.media;
 
-import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-
-import java.util.Arrays;
-
-
 public class MainActivity extends AppCompatActivity {
+    private RelativeLayout rl;
+    private MyVideoView videoView;
+    public static final int PICK_VIDEO = 9;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        check();
-        initView();
-    }
 
-    private void initView() {
-        open(R.id.button1, VideoActivity.class);
-        open(R.id.button3, MediaPlayerActivity.class);
-        open(R.id.button4, IJKPlayerActivity.class);
-        open(R.id.button6, ImageActivity.class);
+        videoView = findViewById(R.id.my_video_view);
+        rl = findViewById(R.id.video_rl);
 
-    }
-    // 检查权限
-
-    public void check() {
-        if (!checkPermissionAllGranted(mPermissionsArrays)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(mPermissionsArrays, REQUEST_PERMISSION);
-            } else {
-
-            }
-        } else {
-            Toast.makeText(MainActivity.this, "已经获取所有所需权限", Toast.LENGTH_SHORT).show();
+        if (getIntent().getData() == null) {
+            return;
         }
+        playVideo(getIntent().getData());
     }
 
-    private void open(int buttonId, final Class<?> clz) {
-        findViewById(buttonId).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, clz));
-            }
-        });
+    private void playVideo(Uri uri) {
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setVideoURI(uri);
+        videoView.start();
     }
 
-
-    private String[] mPermissionsArrays = new String[]{Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO};
-
-    private final static int REQUEST_PERMISSION = 123;
-
-    private boolean checkPermissionAllGranted(String[] permissions) {
-        // 6.0以下不需要
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        for (String permission : permissions) {
-            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
-                // 只要有一个权限没有被授予, 则直接返回 false
-                return false;
-            }
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_menu, menu);
         return true;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_PERMISSION) {
-            Toast.makeText(this, "已经授权" + Arrays.toString(permissions), Toast.LENGTH_LONG).show();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ADD_VIDEO://添加文件管理内的视频
+                PickVideo();
+                break;
+            case R.id.LOAD_VIDEO://加载内置的示例视频
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.bytedance);
+                playVideo(uri);
+                break;
+        }
+        return true;
+    }
+
+    private void PickVideo() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/*");
+        startActivityForResult(intent, MainActivity.PICK_VIDEO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_VIDEO && data != null) {
+            playVideo(data.getData());
         }
     }
+
+    /**
+     * 切换横竖屏的关键代码，需要预先修改AndroidManifest.xml添加相关横竖屏属性
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getWindow().clearFlags((WindowManager.LayoutParams.FLAG_FULLSCREEN));
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT
+                    , dip2px(this, 235f));
+            params.addRule(RelativeLayout.CENTER_IN_PARENT);
+            videoView.setLayoutParams(params);
+            getSupportActionBar().show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            videoView.setLayoutParams(new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT
+                    , LinearLayout.LayoutParams.MATCH_PARENT));
+            getSupportActionBar().hide();
+        }
+    }
+
+    /**
+     * 根据手机分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
 }
---
+++ app/src/main/java/com/android/media/MyVideoView.java
@@ -0,0 +1,23 @@
+package com.android.media;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+public class MyVideoView extends VideoView {
+    public MyVideoView(Context context) {
+        super(context);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getDefaultSize(getWidth(), widthMeasureSpec);
+        int height = getDefaultSize(getHeight(), heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+}
